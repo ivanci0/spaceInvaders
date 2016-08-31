f2d195ec5c4e5c2ae833acc0a730d68eaf115229
santods <sanraro15@hotmail.com>
2016-08-31 20:38:43 -0300

    clase enemigo
    cree la clase enemigo
    organice por carpetas los states y los futuros sprites
    el main directamente llama al play state y no al menu state al final q
    toca la pantalla el enemigo es removido y destruido..

--- source/Main.hx
+++ source/Main.hx
@@ -3,13 +3,15 @@ package;
 import flixel.FlxGame;
 import openfl.Lib;
 import openfl.display.Sprite;
+import states.PlayState;
+
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(160, 144, MenuState, 3)); // ampliado solo para pruebas.//agrego el 3 q es el zoom o escala ya esta
+		addChild(new FlxGame(160, 144,PlayState, 3)); // ampliado solo para pruebas.//agrego el 3 q es el zoom o escala ya esta
 																					 //modificado en el project.xml
 	}
 }
